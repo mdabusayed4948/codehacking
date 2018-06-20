e8526f32d3557fbe65a986df709336dc903e55d6
mdabusayed4948 <mdabusayed4948@gmail.com>
2018-06-21 04:43:08 +0600

    admin master page setup

--- resources/views/admin/index.blade.php
+++ resources/views/admin/index.blade.php
@@ -1,15 +1,16 @@
-@extends('')
-
-@section('title','')
-
-@push('css')
-
-@endpush
+@extends('layouts.admin')
 
 @section('content')
-
+    <div class="row">
+        <div class="col-lg-12">
+            <h1 class="page-header">Admin</h1>
+        </div>
+        <!-- /.col-lg-12 -->
+    </div>
+    <!-- /.row -->
+    <div class="row">
+        <div class="col-lg-12">
+           Admin
+        </div>
+    </div>
 @endsection
-
-@push('scripts')
-
-@endpush
